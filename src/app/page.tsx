c10530397ff629ac85ffd1eff5f2e7d0bb082944
RandomThacker <randomthacker@gmail.com>
2024-05-21 11:40:29 +0530

    Add: Bg hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
+    <>
+    <AuroraBackground/>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         Welcome to postgresLM . Your query friend.
@@ -10,6 +13,7 @@ export default function Home() {
         <Button>Get Started</Button>
       </div>
     </main>
+    </>
   );
 }
 
