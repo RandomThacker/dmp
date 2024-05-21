ea57ef233d3438af98d39ec00aec57073e6bbf82
RandomThacker <randomthacker@gmail.com>
2024-05-21 12:50:40 +0530

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
+import { Spotlight } from "@/components/ui/Spotlight";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-    <AuroraBackground/>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        Welcome to postgresLM . Your query friend.
-      </div>
-
-      <div>
-        <Button>Get Started</Button>
-      </div>
-    </main>
+    <AuroraBackground children="The content to be displayed within the AuroraBackground component"/>
     </>
   );
 }
