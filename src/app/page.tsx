44db140f32b469f7394fa36a194053058727cb6f
RandomThacker <randomthacker@gmail.com>
2024-05-21 11:36:49 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        Welcome to postgresLM . Your query friend.
+      </div>
+
+      <div>
+        <Button>Get Started</Button>
+      </div>
+    </main>
+  );
+}
+
+
