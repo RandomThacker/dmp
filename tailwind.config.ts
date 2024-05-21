78866d44416e919cf5655b3211263b2b07a85724
RandomThacker <randomthacker@gmail.com>
2024-05-21 12:50:07 +0530

    updated tailwing config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,7 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+    
         aurora: {
           from: {
             backgroundPosition: "50% 50%, 50% 50%",
@@ -84,6 +85,7 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         aurora: "aurora 60s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
     },
   },
